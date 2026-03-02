df616265d42e9d46e5817c3ce4a629ebd20926c2
solomon <ohiozeberyl2000@gmail.com>
2026-03-02 17:57:59 +0100

    github action aws container proj4vii

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -2,12 +2,10 @@ variable "vpc_id" {
   type        = string # Added type
   description = "The ID of the VPC where web servers will be deployed."
 }
-
 variable "env_prefix"{
   type = string
   description = "ENVIRONMENT PREFIX"
 }
-
 variable "az_count" {
   default = 2
   type = number
@@ -18,36 +16,29 @@ variable "az_count" {
 variable "execution_role_arn" { 
   type = string 
 }
-
 variable "task_role_arn"      { 
   type = string 
 }
-
 variable "container_image"    { 
   description = "Full ECR image URI with tag"
   type = string 
 }
-
 variable "target_group_arn"   { 
   type = string 
 }
-
 variable "alb_security_group_id" { 
   type = string 
 }
-
 variable "private_subnet_ids" { 
   type = list(string) 
 }
-
-# variable "ecs_sg_id" { 
-#   type =  string 
-# }
-
 variable "domain_name" { 
   type =  string 
 }
-
 variable "alb_listener_arn" { 
   type = string 
+}
+variable "repository_url" {
+  type        = string
+  description = "The URL of the ECR repository from the ECR module"
 }
